2ff916529afdc4b4de95cdb1f7af85a019da17f4
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-31 10:11:29 +0400

    deleting unnecessary files and folder

--- ProductBase_class.py
+++
@@ -1,9 +0,0 @@
-#Product base class
-from abc import ABC, abstractmethod
-
-class ProductBase(ABC):
-    @abstractmethod
-    def __init__(self):
-        pass
-    def display_details(self):
-        pass
